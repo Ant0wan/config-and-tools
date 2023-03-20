6400b0a281d645e73675f903b046e8ba3b861e3c
Antoine Barthelemy <antoine@linux.com>
2023-03-20 16:41:45 +0100

    Update python.sh

--- Fedora/DellXPS13/python.sh
+++ Fedora/DellXPS13/python.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
-set -x
-sudo dnf install -y python2 python3 pip
-python3 -m pip install --upgrade pip
-python3 -m pip install virtualenvwrapper
-mkdir -p "$HOME/.virtualenv"
-grep -a "export WORKON_HOME=$HOME/.virtualenvs" "$HOME/.$(basename "$SHELL")rc" || printf "export WORKON_HOME=%s/.virtualenvs\n" "$HOME" >> "$HOME/.$(basename "$SHELL")rc"
-export WORKON_HOME=$HOME/.virtualenvs
-local pathvirt
-pathvirt="$(which virtualenvwrapper.sh)"
-grep -a "source $pathvirt" "$HOME/.$(basename "$SHELL")rc" || printf "source %s\n" "$pathvirt" >> "$HOME/.$(basename "$SHELL")rc"
-. "$pathvirt"
+set -o errexit
+set -o errtrace
+set -o pipefail
+sudo dnf install -y tk-devel readline-devel sqlite-devel
+curl https://pyenv.run | bash
+printf '
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+' >> "$HOME/.$(basename "$SHELL")rc"
+source "$HOME/.$(basename "$SHELL")rc"
+pyenv install 2.7
+pyenv install 3.11
+pyenv shell 3.11
+pyenv local 3.11
+pyenv global 3.11
+python -m ensurepip --upgrade
 python3 -m pip config set global.require-virtualenv True
-sudo python3 -m pip config --site set global.index-url "https://pypi.org/simple"
