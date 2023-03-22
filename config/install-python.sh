fd0c42a03201e80e44da750e3d88984cad15de54
Antoine Barthelemy <antoine@linux.com>
2023-03-23 00:23:20 +0100

    bin

--- config/install-python.sh
+++ config/install-python.sh
@@ -3,9 +3,6 @@ set -o errexit
 set -o errtrace
 set -o pipefail
 
-which dnf 2>/dev/null && sudo dnf install -y tk-devel readline-devel sqlite-devel
-which apt 2>/dev/null && sudo apt install -y tk-dev readline-dev sqlite-dev
-
 curl https://pyenv.run | bash
 printf '
 export PYENV_ROOT="$HOME/.pyenv"
@@ -13,7 +10,6 @@ command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 ' >> "$HOME/.$(basename "$SHELL")rc"
 source "$HOME/.$(basename "$SHELL")rc"
-pyenv install 2.7
 pyenv install 3.11
 pyenv shell 3.11
 pyenv local 3.11
