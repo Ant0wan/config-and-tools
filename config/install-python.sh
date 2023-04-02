97cb5d24bfef985312a0fdf78f28ebd0a8ee50b3
Antoine Barthelemy <antoine@linux.com>
2023-04-02 19:50:43 +0200

    feat(python)

--- config/install-python.sh
+++ config/install-python.sh
@@ -4,17 +4,12 @@ set -o errtrace
 set -o pipefail
 
 curl https://pyenv.run | bash
-printf '
-export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-' >> "$HOME/.$(basename "$SHELL")rc"
-source "$HOME/.$(basename "$SHELL")rc"
+. "$HOME/.$(basename "$SHELL")rc"
 pyenv install 3.11
 pyenv shell 3.11
 pyenv local 3.11
 pyenv global 3.11
 
-python -m ensurepip --upgrade
-pip config --global set global.require-virtualenv True
-pip config debug
+#python -m ensurepip --upgrade
+#pip config --global set global.require-virtualenv True
+#pip config debug
