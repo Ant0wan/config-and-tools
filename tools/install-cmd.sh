87b8bf4f662d8f7613ff59316130f9f5eacb9e66
Antoine Barthelemy <antoine@linux.com>
2023-04-11 00:07:42 +0200

    rust ok

--- tools/install-cmd.sh
+++ tools/install-cmd.sh
@@ -3,5 +3,5 @@ set -o errexit
 set -o errtrace
 set -o pipefail
 
-BINS=('shellcheck' 'hadolint')
+BINS=('shellcheck' 'hadolint' 'jq')
 sudo dnf install ${BINS[@]} -y
--- tools/install-jq.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-sudo apt install jq -yq
-sudo dnf install jq -yq
