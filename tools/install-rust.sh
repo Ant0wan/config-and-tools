e6d737738cb3aec3c6d3e0763641347802f68bc8
Antoine Barthelemy <antoine@linux.com>
2023-04-11 00:01:47 +0200

    rust ok

--- tools/install-rust.sh
+++ tools/install-rust.sh
@@ -5,5 +5,5 @@ set -o pipefail
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --profile complete -y --no-modify-path
 pushd "$(git rev-parse --show-toplevel)" || exit 1
-cp config/bashrc.d/rust "$HOME"/bashrc.d/rust
+cp config/bashrc.d/rust "$HOME"/.bashrc.d/rust
 popd || exit 1
