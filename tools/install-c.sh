82626fe76cd1fde376a28620c6fa55c422c36b25
Antoine Barthelemy <antoine@linux.com>
2023-04-17 00:02:17 +0200

    install c

--- tools/install-c.sh
+++ tools/install-c.sh
@@ -4,3 +4,6 @@ set -o errtrace
 set -o pipefail
 
 curl 'https://raw.githubusercontent.com/torvalds/linux/master/.clang-format' -o "$HOME/.clang-format"
+pushd "$(git rev-parse --show-toplevel)" || exit 1
+cp config/bashrc.d/c "$HOME"/.bashrc.d/c
+popd || exit 1
