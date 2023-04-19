36d7d6535e759a7913002b095624d0467217addd
Antoine Barthelemy <antoine@linux.com>
2023-04-19 23:44:03 +0200

    del

--- tools/my-tfswitch/target/debug/deps/my_tfswitch-21e3704b29bb4b97.d
+++
@@ -1,5 +0,0 @@
-/home/antoine/config-and-tools/tools/my-tfswitch/target/debug/deps/my_tfswitch-21e3704b29bb4b97: src/main.rs
-
-/home/antoine/config-and-tools/tools/my-tfswitch/target/debug/deps/my_tfswitch-21e3704b29bb4b97.d: src/main.rs
-
-src/main.rs:
--- tools/my-tfswitch/target/release/deps/my_tfswitch-c09f4ab77393fa42.d
+++
@@ -1,5 +0,0 @@
-/home/antoine/config-and-tools/tools/my-tfswitch/target/release/deps/my_tfswitch-c09f4ab77393fa42: src/main.rs
-
-/home/antoine/config-and-tools/tools/my-tfswitch/target/release/deps/my_tfswitch-c09f4ab77393fa42.d: src/main.rs
-
-src/main.rs:
