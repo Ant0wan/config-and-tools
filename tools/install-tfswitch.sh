3948e34d7cc90f114dc6cb34e5c06508106f91df
Antoine Barthelemy <antoine@linux.com>
2023-04-19 22:42:45 +0200

    install terraform

--- tools/install-tfswitch.sh
+++ tools/install-tfswitch.sh
@@ -5,3 +5,8 @@ set -o pipefail
 
 curl -L https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | sudo bash
 
+mkdir -p "$HOME/.bashrc.d/"
+pushd "$(git rev-parse --show-toplevel)" || exit 1
+cp config/bashrc.d/terraform "$HOME"/.bashrc.d/terraform
+popd || exit 1
+
