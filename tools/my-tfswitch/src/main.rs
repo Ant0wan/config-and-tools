f605e36a4cda7f37c195226ff94f22152ecca8a6
Antoine Barthelemy <antoine@linux.com>
2023-04-19 23:43:39 +0200

    fixigin myswitch

--- tools/my-tfswitch/src/main.rs
+++ tools/my-tfswitch/src/main.rs
@@ -1,17 +1,22 @@
+use std::env;
 use std::io::Result;
 use std::io::{self, Write};
 use std::process::Command;
 
 fn main() -> Result<()> {
+    let args: Vec<String> = env::args().skip(1).collect();
+
     let _ = Command::new("/usr/local/bin/tfswitch")
         .output()
         .expect("failed to get Terraform binary");
 
+    println!("{:?}", args);
     let output = Command::new("terraform")
-        .arg("-v")
+        .args(args)
         .output()
         .expect("failed to get Terraform binary");
     io::stdout().write_all(&output.stdout).unwrap();
+    io::stdout().write_all(&output.stderr).unwrap();
 
     Ok(())
 }
---
+++ tools/my-tfswitch/target/release/my-tfswitch.d
@@ -0,0 +1,1 @@
+/home/antoine/config-and-tools/tools/my-tfswitch/target/release/my-tfswitch: /home/antoine/config-and-tools/tools/my-tfswitch/src/main.rs
