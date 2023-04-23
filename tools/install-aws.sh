a89d68b09d8bf1ff4f42e4daee002ec9ed1230fb
Antoine Barthelemy <antoine@linux.com>
2023-04-23 23:20:32 +0200

    feat(aws)

--- tools/install-aws.sh
+++ tools/install-aws.sh
@@ -3,11 +3,12 @@ set -o errexit
 set -o errtrace
 set -o pipefail
 
-command -v pip 2>/dev/null && pip install aws-shell
-
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+rm awscliv2.zip
+
+#command -v pip 2>/dev/null && pip install aws-shell
 
 #pushd "$(git rev-parse --show-toplevel)" || exit 1
 #cp config/bashrc.d/aws "$HOME"/.bashrc.d/aws
