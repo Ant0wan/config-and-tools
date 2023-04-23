64966d5c46c0223a51a69ee6b210adb738998125
Antoine Barthelemy <antoine@linux.com>
2023-04-23 23:27:59 +0200

    add aws

--- tools/install-aws.sh
+++ tools/install-aws.sh
@@ -6,11 +6,9 @@ set -o pipefail
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
-rm awscliv2.zip
+rm -rf awscliv2.zip aws/
 
-#command -v pip 2>/dev/null && pip install aws-shell
-
-#pushd "$(git rev-parse --show-toplevel)" || exit 1
-#cp config/bashrc.d/aws "$HOME"/.bashrc.d/aws
-#popd || exit 1
+pushd "$(git rev-parse --show-toplevel)" || exit 1
+cp config/bashrc.d/aws "$HOME"/.bashrc.d/aws
+popd || exit 1
 
