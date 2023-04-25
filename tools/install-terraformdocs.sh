2f9257b77c14be920513c5e3ce60fbb2eaffc721
Antoine Barthelemy <antoine@linux.com>
2023-04-25 11:09:05 +0200

    terraform docs

--- tools/install-terraformdocs.sh
+++ tools/install-terraformdocs.sh
@@ -5,10 +5,10 @@ set -o pipefail
 
 VERSION='0.16.0'
 
-curl -sSLo ./terraform-docs.tar.gz "https://terraform-docs.io/dl/v${VERISON}/terraform-docs-v${VERSION}-$(uname)-amd64.tar.gz"
+curl -sSLo ./terraform-docs.tar.gz "https://terraform-docs.io/dl/v${VERSION}/terraform-docs-v${VERSION}-$(uname)-amd64.tar.gz"
 tar -xzf terraform-docs.tar.gz
 sudo install terraform-docs /usr/local/bin/
-rm terraform-docs terraform-docs.tar.gz
+rm terraform-docs terraform-docs.tar.gz LICENSE README.md
 
 mkdir -p "$HOME/.bashrc.d/"
 pushd "$(git rev-parse --show-toplevel)" || exit 1
