452177b5c36e9b0ef97c24541044f906ca87a003
Antoine Barthelemy <antoine@linux.com>
2023-06-06 18:25:10 +0200

    update provider

--- tools/hclq.install.sh
+++ tools/hclq.install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
-curl -sSL https://install.hclq.sh | sh
+wget -qO- https://install.hclq.sh | sh
 mkdir -p "$HOME/.bashrc.d/"
 pushd "$(git rev-parse --show-toplevel)" || exit 1
 cp config/bashrc.d/hclq "$HOME"/.bashrc.d/hclq
