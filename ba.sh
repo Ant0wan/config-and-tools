c4f6d7c58dedadff9dd3859d42ee291ff02c8de6
Antoine Barthelemy <antoine@linux.com>
2023-06-07 14:02:48 +0200

    new intelligent script

---
+++ ba.sh
@@ -0,0 +1,7 @@
+info=$(curl https://api.github.com/repos/sharkdp/bat/releases/latest | jq .tag_name,.id -r)
+tag=$(echo $info | awk -F ' ' '{ print $1 }')
+id=$(echo $info | awk -F ' ' '{ print $2 }')
+echo "tag=$tag"
+echo "id=$id"
+#pkgs=$(curl https://api.github.com/repos/sharkdp/bat/releases/${info[1]}/assets | jq .[].name -r)
+#echo $pkgs
