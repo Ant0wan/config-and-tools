646d8a2030af21027915b1121f1795d3e4f2f9bb
Antoine Barthelemy <antoine@linux.com>
2023-06-08 00:14:05 +0200

    hjop

--- tools/vim.install.sh
+++ tools/vim.install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 set -o errexit
 set -o nounset
-VERSION="8.2"
-curl -O https://ftp.nluug.nl/pub/vim/unix/vim-${VERSION}.tar.gz
-tar -xzvf vim-${VERSION}.tar.gz
+VERSION="v8.2.3451"
+curl -LO https://github.com/vim/vim/archive/refs/tags/${VERSION}.tar.gz
+tar -xzvf ${VERSION}.tar.gz
+cd ${VERSION}/
+./configure --prefix=/usr/local
+make
+sudo make install
 rm -rf "$HOME"/.vim
 git clone git@github.com:Ant0wan/vim-plugin.git "$HOME"/.vim/
