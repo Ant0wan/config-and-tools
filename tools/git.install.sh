eca085abbf2047cc1a80a79ce9d583ec5f20e1e0
Antoine Barthelemy <antoine@linux.com>
2023-06-08 01:39:54 +0200

    test split

--- tools/git.install.sh
+++ tools/git.install.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -o errexit
 set -o nounset
+VESRION=$(curl -s https://api.github.com/repos/git/git/git/refs/tags | jq -r '.[].ref' | awk -F'/' '{print $3}' | sort -V | tail -1)
+curl -L https://github.com/git/git/archive/$VERSION.tar.gz -o git.tar.gz
+tar -xzvf git.tar.gz
+cd git-*
+make prefix=/usr/local all
+sudo make prefix=/usr/local install
+rm -rf git.tar.gz git-$VERSION
 githubsource="https://raw.githubusercontent.com/Ant0wan/config-and-tools/main/config/"
 wget "${githubsource}gitconfig" -O "$HOME"/.gitconfig
 wget "${githubsource}gitignore" -O "$HOME"/.gitignore
