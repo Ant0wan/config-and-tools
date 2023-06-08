79ca6e766a79a70a1564fc1c7efe50b664cd9d8f
Antoine Barthelemy <antoine@linux.com>
2023-06-08 14:43:04 +0200

    update

--- init.sh
+++ init.sh
@@ -2,29 +2,28 @@
 set -o errexit
 
 _prompt() {
-	#wget -q -O -  https://raw.githubusercontent.com/lotabout/skim/master/install | sh
-	#info=$(curl https://api.github.com/repos/sharkdp/bat/releases/latest | jq .tag_name,.id -r)
-	#tag=$(echo $info | awk -F ' ' '{ print $1 }')
-	#id=$(echo $info | awk -F ' ' '{ print $2 }')
-	#pkgs=$(curl https://api.github.com/repos/sharkdp/bat/releases/${id}/assets | jq .[].name -r)
-	#kernel=$(uname -s | awk '{print tolower($0)}')
-	#arch=$(uname -p)
-	#list=$(echo "$pkgs" | grep "$kernel" | grep "$arch")
-	#target=""
-	#if echo "$list" | grep -q "musl" 2>&1 >/dev/null; then
-	#	target=$(echo "$list" | grep "musl")
-	#else
-	#	target=$(echo "$list" | grep "gnu")
-	#fi
-	#wget https://github.com/sharkdp/bat/releases/latest/download/$target
-	#tar -xvf $target
-	#folder="$(echo $target | awk -F '.tar.gz' '{ print $1 }')"
-	#cp ${folder}/bat bin/bat
-	#rm $folder $target -rf
+	wget -q -O -  https://raw.githubusercontent.com/lotabout/skim/master/install | sh
+	info=$(curl https://api.github.com/repos/sharkdp/bat/releases/latest | jq .tag_name,.id -r)
+	tag=$(echo $info | awk -F ' ' '{ print $1 }')
+	id=$(echo $info | awk -F ' ' '{ print $2 }')
+	pkgs=$(curl https://api.github.com/repos/sharkdp/bat/releases/${id}/assets | jq .[].name -r)
+	kernel=$(uname -s | awk '{print tolower($0)}')
+	arch=$(uname -p)
+	list=$(echo "$pkgs" | grep "$kernel" | grep "$arch")
+	target=""
+	if echo "$list" | grep -q "musl" 2>&1 >/dev/null; then
+		target=$(echo "$list" | grep "musl")
+	else
+		target=$(echo "$list" | grep "gnu")
+	fi
+	wget https://github.com/sharkdp/bat/releases/latest/download/$target
+	tar -xvf $target
+	folder="$(echo $target | awk -F '.tar.gz' '{ print $1 }')"
+	cp ${folder}/bat bin/bat
+	rm $folder $target -rf
 	selection=$(find tools/ -type f -printf "%f\n" | awk -F '.' '{ print $1 }' | bin/sk --multi --bind 'right:select-all,left:deselect-all,space:toggle+up' --preview="bin/bat --color=always tools/{}.install.sh --color=always")
-	#rm bin/ -rf
+	rm bin/ -rf
 }
-
 
 if test $# -eq 0; then
 	_prompt
@@ -32,18 +31,18 @@ else
 	selection="$(echo $@ | tr ' ' '\n' | sort -u | tr '\n' ' ' | xargs echo | sort)"
 fi
 echo ${selection}
-exit
-
 
 for i in $selection
 do
-	echo 'sudo sh "tools/$i.install.sh"'
+	if test -e "tools/$i.install.sh"; then
+		echo sudo sh "tools/$i.install.sh"
+	fi
 	if test -e "bashrc.d/$i"; then
 		mkdir -p "$HOME/.bashrc.d/"
-		echo 'cp "bashrc.d/$i" "$HOME/.bashrc.d/$i"'
+		echo cp "bashrc.d/$i" "$HOME/.bashrc.d/$i"
 	fi
 done
-#if test -n "$BW_SESSION"; then
-#    bw logout
-#    unset BW_SESSION
-#fi
+if test -n "$BW_SESSION"; then
+    bw logout
+    unset BW_SESSION
+fi
