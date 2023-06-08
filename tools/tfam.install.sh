cded911f65cea439628c9efcc833530f2fcf7775
Antoine Barthelemy <antoine@linux.com>
2023-06-08 15:09:11 +0200

    go test

--- tools/tfam.install.sh
+++ tools/tfam.install.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 set -o errexit
-set -o nounset
-wget https://github.com/Ant0wan/tfam/releases/latest/download/tfam -O /usr/local/bin/tfam
-chmod +x /usr/local/bin/tfam
-mkdir -p "$HOME/.bashrc.d/"
+wget -q -O - https://raw.githubusercontent.com/Ant0wan/tfam/master/install.sh | sh
