73f615b323ba11ad3ccf7909bb4eb9ca0da5aab0
Antoine Barthelemy <antoine@linux.com>
2023-06-27 20:47:22 +0200

    update openssl

--- tools/git.install.sh
+++ tools/git.install.sh
@@ -1,39 +1,21 @@
 #!/bin/sh
-# Ensure that the required dependencies are installed before proceeding with the installation
-if ! command -v openssl &>/dev/null; then
-    echo "OpenSSL is required. Please install it first."
-    script_dir=$(dirname "$0")
-    sh "$script_dir/openssl.install.sh"
-    exit 1
+
+# Check if the script is running as root
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Please run this script as root."
+  exit 1
 fi
 
-# Rest of the script to install Git
-exit 1
-
-
-set -o errexit
-VERSION=$(curl -s https://api.github.com/repos/git/git/git/refs/tags | jq -r '.[].ref' | awk -F'/' '{print $3}' | sort -V | tail -1 | sed 's/^v//')
-curl -L "https://github.com/git/git/archive/v${VERSION}.tar.gz" -o git.tar.gz
-tar -xzvf git.tar.gz
-cd git-*
-make prefix=/usr/local all
-sudo make prefix=/usr/local install
-rm -rf git.tar.gz "git-v${VERSION}"
-githubsource="https://raw.githubusercontent.com/Ant0wan/config-and-tools/main/config/"
-wget "${githubsource}gitconfig" -O "$HOME"/.gitconfig
-wget "${githubsource}gitignore" -O "$HOME"/.gitignore
-if test-z "$BW_SESSION"; then
-    BW_SESSION=$(bw login --raw)
-    export BW_SESSION
+# Check if the distribution is Fedora or Ubuntu
+if [ -f /etc/fedora-release ]; then
+  # Install git on Fedora
+  dnf install -y git
+elif [ -f /etc/lsb-release ]; then
+  # Install git on Ubuntu
+  apt-get update
+  apt-get install -y git
+else
+  echo "Unsupported distribution. This script only supports Fedora and Ubuntu."
+  exit 1
 fi
-_jq() {
-	echo "${key}" | base64 --decode | jq -r "${1}"
-}
-
-for key in $(bw get item gpg | jq -r '.fields[] | @base64'); do
-	_jq '.value' | base64 --decode >"$(_jq '.name')"
-	SIGNING_KEY="${SIGNING_KEY:+$SIGNING_KEY }$(gpg --import "$(_jq '.name')" 2>&1 | head -n 1 | grep -Eo '[0-9A-Z]{16}+')"
-	export SIGNING_KEY
-	rm "$(_jq '.name')"
-done
-sed -i "s/{{signing_key}}/${SIGNING_KEY%% *}/g" "$HOME"/.gitconfig
+echo "Git has been successfully installed."
