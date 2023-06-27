28fe1edf064cf90097395f89b8e97fbd6ac75927
Antoine Barthelemy <antoine@linux.com>
2023-06-27 22:11:21 +0200

    python already installed

--- tools/python.install.sh
+++ tools/python.install.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
+VERSION='3.11'
+if /usr/bin/python --version | grep $VERSION; then
+	echo "Python already installed."
+	exit 0
+fi
 script_dir=$(dirname "$0")
 . "$script_dir/essentials.install.sh"
 curl https://pyenv.run | bash
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
-pyenv install 3.11
-pyenv shell 3.11
-#pyenv local 3.11
-pyenv global 3.11
+pyenv install $VERSION
+pyenv shell $VERSION
+#pyenv local $VERSION
+pyenv global $VERSION
 python -m ensurepip --upgrade
 pip install autopep8
 pip install pycodestyle
