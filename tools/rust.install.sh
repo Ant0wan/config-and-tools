735e6d8198b51a2ec8afb42a91f2e279e21bfceb
Antoine Barthelemy <antoine@linux.com>
2023-07-14 23:00:19 +0200

    Update rust.install.sh

--- tools/rust.install.sh
+++ tools/rust.install.sh
@@ -4,6 +4,6 @@ script_dir=$(dirname "$0")
 . "$script_dir/essentials.install.sh"
 . "$script_dir/c.install.sh"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --profile complete -y --no-modify-path
-source "$HOME/.cargo/env"
+. "$HOME/.cargo/env"
 "$(which cargo)" install cargo-watch
 "$(which cargo)" install cargo-audit
