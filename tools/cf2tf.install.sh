103030d8fb29ca455cf3d923453bfe00d29b8d1f
Antoine Barthelemy <antoine@linux.com>
2023-09-17 21:19:28 +0200

    feat(cf2tf)

--- tools/cf2tf.install.sh
+++ tools/cf2tf.install.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-if [ -f /etc/fedora-release ]; then
-  sudo dnf install -y cf2tf
-elif [ -f /etc/lsb-release ] || [ -f /etc/os-release ] ; then
-  sudo apt-get update
-  sudo apt-get install -y cf2tf
-else
-  echo "Unsupported distribution. This script only supports Fedora and Ubuntu."
-  exit 1
-fi
-echo "Cloudformation-to-Terraform has been successfully installed."
+script_dir=$(dirname "$0")
+. "$script_dir/python.install.sh"
+pip install cf2tf
