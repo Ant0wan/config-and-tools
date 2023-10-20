42923259368d5769e93536a1cd3bdf33eb514cc0
Antoine Barthelemy <antoine@linux.com>
2023-10-20 15:07:07 +0200

    shell lint ok

--- init.sh
+++ init.sh
@@ -2,24 +2,24 @@
 set -o errexit
 _prompt() {
 	wget -q -O -  https://raw.githubusercontent.com/lotabout/skim/master/install | sh
-	info=$(curl https://api.github.com/repos/sharkdp/bat/releases/latest | jq .tag_name,.id -r)
-	tag=$(echo $info | awk -F ' ' '{ print $1 }')
-	id=$(echo $info | awk -F ' ' '{ print $2 }')
-	pkgs=$(curl https://api.github.com/repos/sharkdp/bat/releases/${id}/assets | jq .[].name -r)
-	kernel=$(uname -s | awk '{print tolower($0)}')
-	arch=$(uname -m)
-	list=$(echo "$pkgs" | grep "$kernel" | grep "$arch")
+	info="$(curl https://api.github.com/repos/sharkdp/bat/releases/latest | jq .tag_name,.id -r)"
+	#tag="$(echo "$info" | awk -F ' ' '{ print $1 }')"
+	id="$(echo "$info" | awk -F ' ' '{ print $2 }')"
+	pkgs="$(curl "https://api.github.com/repos/sharkdp/bat/releases/${id}/assets" | jq .[].name -r)"
+	kernel="$(uname -s | awk '{print tolower($0)}')"
+	arch="$(uname -m)"
+	list="$(echo "$pkgs" | grep "$kernel" | grep "$arch")"
 	target=""
-	if echo "$list" | grep -q "musl" 2>&1 >/dev/null; then
-		target=$(echo "$list" | grep "musl")
+	if echo "$list" | grep -q "musl" >/dev/null 2>&1; then
+		target="$(echo "$list" | grep "musl")"
 	else
-		target=$(echo "$list" | grep "gnu")
+		target="$(echo "$list" | grep "gnu")"
 	fi
-	wget https://github.com/sharkdp/bat/releases/latest/download/$target
-	tar -xvf $target
-	folder="$(echo $target | awk -F '.tar.gz' '{ print $1 }')"
-	cp ${folder}/bat bin/bat
-	selection=$(find tools/ -type f -printf "%f\n" | awk -F '.' '{ print $1 }' | bin/sk --multi --bind 'right:select-all,left:deselect-all,space:toggle+up' --preview="bin/bat --color=always tools/{}.install.sh --color=always")
+	wget "https://github.com/sharkdp/bat/releases/latest/download/$target"
+	tar -xvf "$target"
+	folder="$(echo "$target" | awk -F '.tar.gz' '{ print $1 }')"
+	cp "${folder}/bat" bin/bat
+	selection="$(find tools/ -type f -printf "%f\n" | awk -F '.' '{ print $1 }' | bin/sk --multi --bind 'right:select-all,left:deselect-all,space:toggle+up' --preview="bin/bat --color=always tools/{}.install.sh --color=always")"
 }
 download_path=$(mktemp -d -t config.XXXXXXXXXX)
 cd "$download_path"
@@ -29,8 +29,8 @@ cd "${download_path}/config-main"
 if test $# -eq 0; then
 	_prompt
 else
-	selection="$(echo $@ | tr ' ' '\n' | sort -u | tr '\n' ' ' | xargs echo | sort)"
-	echo $selection
+	selection="$(echo "$@" | tr ' ' '\n' | sort -u | tr '\n' ' ' | xargs echo | sort)"
+	echo "$selection"
 fi
 for i in $selection; do
 	if test -e "tools/$i.install.sh"; then
