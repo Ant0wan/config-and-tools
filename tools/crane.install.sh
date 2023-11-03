6c9a26368cf86e5aaf5a7201836617c139b8ca1a
Antoine Barthelemy <antoine@linux.com>
2023-11-03 11:50:18 +0100

    feat(install): crane

--- tools/crane.install.sh
+++ tools/crane.install.sh
@@ -3,7 +3,8 @@ set -o errexit
 VERSION=$(curl -s "https://api.github.com/repos/google/go-containerregistry/releases/latest" | jq -r '.tag_name')
 ARCH="$(uname -m)"
 OS="$(uname -s)"
+TARGET="go-containerregistry.tar.gz"
 TMP="/tmp/go-containerregistry"
 mkdir -p "$TMP"
 curl -sL "https://github.com/google/go-containerregistry/releases/download/${VERSION}/go-containerregistry_${OS}_${ARCH}.tar.gz" > "${TMP}/${TARGET}"
-tar -zxvf "${TMP}/${TARGET}" -C /usr/local/bin/ crane
+sudo tar -zxvf "${TMP}/${TARGET}" -C /usr/local/bin/ crane
