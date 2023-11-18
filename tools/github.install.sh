fe41490e0b4fd755ebae3acb2df6b8865a46bddb
Antoine Barthelemy <antoine@linux.com>
2023-11-18 12:31:59 +0100

    wip

--- tools/github.install.sh
+++ tools/github.install.sh
@@ -4,7 +4,7 @@ if [ -f /etc/fedora-release ]; then
   sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
   sudo dnf install gh
 elif [ -f /etc/lsb-release ] || [ -f /etc/os-release ] ; then
-  type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+  command -v curl >/dev/null || (sudo apt update && sudo apt install curl -y)
   curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
   && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
   && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
