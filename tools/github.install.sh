20a995537afd5f91c634f70aaa8bea244b6ec880
Antoine Barthelemy <antoine@linux.com>
2023-11-18 01:08:51 +0100

    wip

---
+++ tools/github.install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+if [ -f /etc/fedora-release ]; then
+  sudo dnf install 'dnf-command(config-manager)'
+  sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+  sudo dnf install gh
+elif [ -f /etc/lsb-release ] || [ -f /etc/os-release ] ; then
+  type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+  && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+  && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+  && sudo apt-get update \
+  && sudo apt-get install -y gh
+else
+  echo "Unsupported distribution. This script only supports Fedora and Ubuntu."
+  exit 1
+fi
+gh extension install github/gh-copilot
+gh extension install nektos/act
+echo "GitHub CLI has been successfully installed."
