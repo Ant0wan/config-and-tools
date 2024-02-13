2d916c0b1380eaecfbd14d931c6cb12189dddbed
Antoine Barthelemy <antoine@linux.com>
2024-02-13 15:00:40 -0500

    updates

--- tools/ssh.install.sh
+++ tools/ssh.install.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 set -o errexit
+if [ -f /etc/fedora-release ]; then
+	sudo dnf install -y ssh
+elif [ -f /etc/lsb-release ] || [ -f /etc/os-release ] ; then
+	sudo apt-get update
+	sudo apt-get install -y ssh
+else
+	echo "Unsupported distribution. This script only supports Fedora and Ubuntu."
+	exit 1
+fi
 if test -e "$HOME"/.ssh/github; then
 	echo "SSH already configure."
 	exit 0
