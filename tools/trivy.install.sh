7c970b6b7b819d9043f0745d5ad9e9edf54bf1c5
Antoine Barthelemy <antoine@linux.com>
2024-03-01 13:53:23 +0100

    feat(install): trivy

--- tools/trivy.install.sh
+++ tools/trivy.install.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+set -o errexit
 VERSION=$(curl -s "https://api.github.com/repos/aquasecurity/trivy/releases/latest" | jq -r '.tag_name')
 SCRIPTNAME="$(basename "$0")"
 BIN="${SCRIPTNAME%.install.sh}"
 TMP="/tmp/${BIN}_${LATEST}"
 OS="$(uname -s)"
 TARGET="trivy_${VERSION:1}_${OS}-64bit.deb"
+mkdir -p "$TMP"
 wget "https://github.com/aquasecurity/trivy/releases/download/${VERSION}/${TARGET}" -O "${TMP}/${TARGET}"
-sudo dpkg -i "${TARGET}"
+sudo dpkg -i "${TMP}/${TARGET}"
