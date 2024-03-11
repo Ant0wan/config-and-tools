f0d2ded86f6a88ef752f34c1ad28e9f2b990b851
Antoine Barthelemy <antoine@linux.com>
2024-03-11 13:22:42 +0100

    update bash only

--- tools/bash.install.sh
+++ tools/bash.install.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 set -o errexit
 set -o nounset
-#githubsource="https://raw.githubusercontent.com/Ant0wan/config-and-tools/main/config/"
+githubsource="https://raw.githubusercontent.com/Ant0wan/config-and-tools/main/config/"
+if test -e ~/.bashrc; then
+	if ! grep -Fxq '	for rc in ~/.bashrc.d/*; do' ~/.bashrc; then
+		printf '
+# User specific aliases and functions
+if [ -d ~/.bashrc.d ]; then
+	for rc in ~/.bashrc.d/*; do
+		if [ -f "$rc" ]; then
+			. "$rc"
+		fi
+	done
+fi
+
+unset rc
+
+' >> ~/.bashrc
+	fi
+
+else
+	if test -e /etc/skel/.bashrc; then
+		cp /etc/skel/.bashrc ~
+	else
+		wget "${githubsource}bashrc" -O "$HOME"/.bashrc
+	fi
+fi
 mkdir -p "$HOME"/.bashrc.d/
