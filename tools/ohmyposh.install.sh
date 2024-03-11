aebff4fddf27b14ca37c65366ea96770ffbf695d
Antoine Barthelemy <antoine@linux.com>
2024-03-11 13:00:12 +0100

    update oh my posh

--- tools/ohmyposh.install.sh
+++ tools/ohmyposh.install.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -o errexit
 set -o nounset
+OS="$(uname -s | awk '{print tolower($0)}')"
+ARCH="$(uname -m | sed 's/x86_64/amd64/')"
 githubsource="https://raw.githubusercontent.com/Ant0wan/config-and-tools/main/config/"
-sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-${OS}-${ARCH} -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
 oh-my-posh font install Meslo
 oh-my-posh get shell
