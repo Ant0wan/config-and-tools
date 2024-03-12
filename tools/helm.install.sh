6ea2c534830d71b661f39e315f141c36ae78e55e
Antoine Barthelemy <antoine@linux.com>
2024-03-12 13:22:02 +0100

    update script

--- tools/helm.install.sh
+++ tools/helm.install.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
-set -o errexit
 #curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+set -o errexit
 SCRIPTNAME="$(basename "$0")"
 BIN="${SCRIPTNAME%.install.sh}"
 VERSION=$(curl -s "https://api.github.com/repos/${BIN}/${BIN}/releases/latest" | jq -r '.tag_name')
 OS="$(uname -s | awk '{print tolower($0)}')"
 ARCH="$(uname -m | sed 's/x86_64/amd64/')"
 TMP="/tmp/${BIN}_${VERSION}"
-wget "https://get.helm.sh/${BIN}-${VERSION}-${OS}-${ARCH}.tar.gz" -O ${TMP}
+TARGET="${BIN}-${VERSION}-${OS}-${ARCH}.tar.gz"
+mkdir -p "${TMP}"
+wget "https://get.helm.sh/${TARGET}" -O "${TMP}/${TARGET}"
+wget "https://get.helm.sh/${TARGET}.sha256sum" -O "${TMP}/${TARGET}.sha256sum"
+SUM="$(shasum --algorithm 256 "${TMP}/${TARGET}" | awk '{print $1}')"
+EXPECTED_SUM="$(cat "${TMP}/${TARGET}.sha256sum" | awk '{print $1}')"
+test "${SUM}" = "${EXPECTED_SUM}" || (echo "SHA sum of ${TARGET} does not match. Aborting."; exit 1)
