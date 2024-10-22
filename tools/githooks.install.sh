0ede1ec600da56962753788381b2ca0bd9c0034b
Antoine Barthelemy <antoine@linux.com>
2024-10-22 13:52:50 +0200

    update

---
+++ tools/githooks.install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -o errexit
+DIR='~/.git-templates'
+mkdir -p "${DIR}/hooks"
+git config --global init.templatedir "${DIR}"
+echo "Copy all hooks for here"
+chmod a+x ~/.git-templates/hooks/*
