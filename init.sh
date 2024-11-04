d1b340a3d33b4ee6fa0839c6724b318b1b978f77
Antoine Barthelemy <antoine@linux.com>
2024-11-04 23:30:25 +0100

    split install

--- init.sh
+++ init.sh
@@ -1,43 +1,7 @@
 #!/bin/bash
-
 set -o errexit
 
-_install_skim() {
-	wget -q -O -  https://raw.githubusercontent.com/lotabout/skim/master/install | bash
-}
-
-_install_bat() {
-	info=$(curl https://api.github.com/repos/sharkdp/bat/releases/latest | jq .tag_name,.id -r)
-	id="$(echo "$info" | awk 'NR==2')"
-	pkgs="$(curl "https://api.github.com/repos/sharkdp/bat/releases/${id}/assets" | jq .[].name -r)"
-	kernel="$(uname -s | awk '{print tolower($0)}')"
-	arch="$(uname -m)"
-	list="$(echo "$pkgs" | grep "$kernel" | grep "$arch")"
-	target=""
-	if echo "$list" | grep -q "musl" >/dev/null 2>&1; then
-		target="$(echo "$list" | grep "musl")"
-	else
-		target="$(echo "$list" | grep "gnu")"
-	fi
-	wget "https://github.com/sharkdp/bat/releases/latest/download/$target"
-	tar -xvf "$target"
-	folder="$(echo "$target" | awk -F '.tar.gz' '{ print $1 }')"
-	cp "${folder}/bat" bin/bat
-}
-
-_prompt() {
-	_install_skim
-	#exit
-	_install_bat
-	selection="$(find scripts/ -type f -printf "%f\n" | awk -F '.' '{ print $1 }' | sort | bin/sk --multi --bind 'right:select-all,left:deselect-all,space:toggle+up' --preview="bin/bat --color=always scripts/{}.sh --color=always")"
-}
-
-download_path=$(mktemp -d -t conf.XXXXXXXXXX)
-cd "$download_path"
-
-wget https://github.com/Ant0wan/conf/archive/refs/heads/main.zip
-unzip main.zip
-cd "${download_path}/conf-main"
+selection="$(find scripts/ -type f -printf "%f\n" | awk -F '.' '{ print $1 }' | sort | bin/sk --multi --bind 'right:select-all,left:deselect-all,space:toggle+up' --preview="bin/bat --color=always scripts/{}.sh --color=always")"
 
 if test $# -eq 0; then
 	_prompt
@@ -47,9 +11,8 @@ else
 fi
 
 for i in $selection; do
-	if test -e "scripts/$i.sh"; then
-		sh "scripts/$i.sh"
-	fi
+	[ -e "scripts/$i.sh" ] && bash "scripts/$i.sh"
+
 	if test -e "bashrc.d/$i"; then
 		mkdir -p "$HOME/.bashrc.d/"
 		cp "bashrc.d/$i" "$HOME/.bashrc.d/$i"
@@ -60,4 +23,5 @@ if test -n "$BW_SESSION"; then
     bw logout
     unset BW_SESSION
 fi
+
 rm -rf "${download_path}"
