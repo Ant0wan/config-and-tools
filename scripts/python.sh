529c5f9d651784e9a39ede075e0c90d38409b15c
Antoine Barthelemy <antoine@linux.com>
2025-02-07 11:02:28 +0100

    feat: python script update

--- scripts/python.sh
+++ scripts/python.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -o errexit
 VERSION='3.12'
-if /usr/bin/python --version | grep $VERSION; then
+if command -v python; then
 	echo "Python already installed."
 	exit 0
 fi
 script_dir=$(dirname "$0")
-. "$script_dir/essentials.install.sh"
+. "$script_dir/essentials.sh"
 curl https://pyenv.run | bash
 export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
@@ -18,6 +18,6 @@ pyenv global $VERSION
 python -m ensurepip --upgrade
 pip install autopep8
 pip install pycodestyle
-#pip config --global set global.require-virtualenv True
+sudo pip config --global set global.require-virtualenv True
 pip config debug
 echo "Python have been successfully installed."
